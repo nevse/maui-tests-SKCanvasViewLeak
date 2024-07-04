676dae0cfa86e167a1bf556b7b30409e7355ebcd
nevse <nov.eugen@gmail.com>
2024-07-04 16:03:48 +0400

    first commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SKCanvasViewLeak;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace SKCanvasViewLeak;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+	WeakReference pageRef;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+	void OnOpenPage(object sender, EventArgs e)
+	{
+		var page = new SKCanvasViewPage();
+		Navigation.PushAsync(page);
+		pageRef = new WeakReference(page);
+	}
+	void OnCheckMemoryLeak(object sender, EventArgs e)
+	{
+		GC.Collect();
+		GC.WaitForPendingFinalizers();
+		GC.Collect();
+		if (pageRef != null && pageRef.IsAlive)
+		{
+			DisplayAlert("Memory Leak", "Memory Leak Detected", "OK");
+		}
+		else
+		{
+			DisplayAlert("Memory Leak", "No Memory Leak Detected", "OK");
+		}
+	}
+}
